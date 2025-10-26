c67b4cac1ead022d474e260e8389a6d4bc52794e
Sahaj33 <sahajitaliya07@gmail.com>
2025-10-26 15:15:42 +0530

    chore: scaffold unified package layout and proxy main.py to msm.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+MSM Unified Entrypoint (scaffold)
+- Temporarily proxies to msm.py while modular extraction proceeds
+"""
+from msm import main
+
+if __name__ == "__main__":
+    main()
